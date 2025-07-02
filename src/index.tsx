2de03138b88409fdbed81f6bbcd7a3a75e6158d4
cyan0714 <917244295@qq.com>
2025-07-03 07:08:53 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter, Link, Outlet } from 'react-router-dom'
+import { Index } from './pages/Index/Index'
+import { ErrorPage } from './pages/ErrorPage/ErrorPage'
+import { UserManage } from './pages/UserManage/UserManage'
+import { Login } from './pages/Login/Login'
+import { Menu } from './pages/Menu/Menu'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const routes = [
+  {
+    path: "/",
+    element: <Index></Index>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Menu></Menu>,
+        children: [
+          {
+            path: 'user_manage',
+            element: <UserManage/>
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: "login",
+    element: <Login />,
+  }
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<RouterProvider router={router} />)
